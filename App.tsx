8c35ae636d3f94052899b718592866bccf542466
fotis tsakiris <footios76@gmail.com>
2020-11-07 12:17:08 +0200

    show Onbarding initial screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
+import Onboarding from './src/Authentication/Onbording'
+
+const AuthenticationStack = createStackNavigator();
+
 export default function App() {
+
+
+
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <AuthenticationStack.Navigator>
+        <AuthenticationStack.Screen name='Onboarding' component={Onboarding} />
+      </AuthenticationStack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ src/Authentication/Onbording/Onboarding.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+import { View } from "react-native";
+
+export interface OnboardingProps {
+
+}
+
+
+export interface OnboardingProps {
+
+}
+
+const Onboarding: React.SFC<OnboardingProps> = () => {
+  return (<View style={{ flex: 1, backgroundColor: 'cyan' }} ></View>);
+}
+
+export default Onboarding;
